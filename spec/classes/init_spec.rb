b66b5a317f44a921b5aa276976ecccfe741be92b
Stefan Möding <stm@kill-9.net>
2018-06-28 18:36:49 +0200

    Convert module to PDK

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -9,15 +9,15 @@ describe 'resolv_conf' do
         is_expected.to contain_class('resolv_conf')
       }
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
@@ -27,15 +27,15 @@ describe 'resolv_conf' do
       end
 
       it {
-        is_expected.to contain_file('/run/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/run/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
@@ -45,96 +45,96 @@ describe 'resolv_conf' do
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('owner').
-          with_group('group').
-          with_mode('1246').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('owner')
+          .with_group('group')
+          .with_mode('1246')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with parameter sortlist" do
       let(:params) do
-        { sortlist: %w[foo bar] }
+        { sortlist: ['foo', 'bar'] }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          with_content(%r{^sortlist foo bar$}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .with_content(%r{^sortlist foo bar$})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with one nameserver" do
       let(:params) do
-        { nameservers: %w[8.8.8.8] }
+        { nameservers: ['8.8.8.8'] }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 8.8.8.8$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 8.8.8.8$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with two nameservers" do
       let(:params) do
-        { nameservers: %w[8.8.8.8 8.8.4.4] }
+        { nameservers: ['8.8.8.8', '8.8.4.4'] }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 8.8.8.8$}).
-          with_content(%r{^nameserver 8.8.4.4$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 8.8.8.8$})
+          .with_content(%r{^nameserver 8.8.4.4$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with three nameservers" do
       let(:params) do
-        { nameservers: %w[8.8.8.8 8.8.4.4 9.9.9.9] }
+        { nameservers: ['8.8.8.8', '8.8.4.4', '9.9.9.9'] }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 8.8.8.8$}).
-          with_content(%r{^nameserver 8.8.4.4$}).
-          with_content(%r{^nameserver 9.9.9.9$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 8.8.8.8$})
+          .with_content(%r{^nameserver 8.8.4.4$})
+          .with_content(%r{^nameserver 9.9.9.9$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with four nameservers" do
       let(:params) do
-        { nameservers: %w[8.8.8.8 8.8.4.4 9.9.9.9 9.9.9.10] }
+        { nameservers: ['8.8.8.8', '8.8.4.4', '9.9.9.9', '9.9.9.10'] }
       end
 
       it {
@@ -144,85 +144,85 @@ describe 'resolv_conf' do
 
     context "on #{os} with truthy prepend_local_nameserver" do
       let(:params) do
-        { nameservers: %w[8.8.8.8], prepend_local_nameserver: true }
+        { nameservers: ['8.8.8.8'], prepend_local_nameserver: true }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          with_content(%r{^nameserver 8.8.8.8$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .with_content(%r{^nameserver 8.8.8.8$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with three nameservers and truthy prepend_local_nameserver" do
       let(:params) do
         {
-          nameservers: %w[8.8.8.8 8.8.4.4 9.9.9.9],
-          prepend_local_nameserver: true
+          nameservers: ['8.8.8.8', '8.8.4.4', '9.9.9.9'],
+          prepend_local_nameserver: true,
         }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          with_content(%r{^nameserver 8.8.8.8$}).
-          with_content(%r{^nameserver 8.8.4.4$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .with_content(%r{^nameserver 8.8.8.8$})
+          .with_content(%r{^nameserver 8.8.4.4$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with falsy prepend_local_nameserver" do
       let(:params) do
-        { nameservers: %w[8.8.8.8], prepend_local_nameserver: false }
+        { nameservers: ['8.8.8.8'], prepend_local_nameserver: false }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 8.8.8.8$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 8.8.8.8$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with parameter searchlist" do
       let(:params) do
-        { searchlist: %w[foo bar] }
+        { searchlist: ['foo', 'bar'] }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          without_content(%r{^domain}).
-          with_content(%r{^search foo bar$}).
-          without_content(%r{^sortlist$}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .without_content(%r{^domain})
+          .with_content(%r{^search foo bar$})
+          .without_content(%r{^sortlist$})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with too many searchlist items" do
       let(:params) do
-        { searchlist: %w[foo bar baz qux quux quuz corge] }
+        { searchlist: ['foo', 'bar', 'baz', 'qux', 'quux', 'quuz', 'corge'] }
       end
 
       it {
@@ -236,21 +236,21 @@ describe 'resolv_conf' do
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          with_content(%r{^domain example\.com$}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist$}).
-          without_content(%r{^options})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .with_content(%r{^domain example\.com$})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist$})
+          .without_content(%r{^options})
       }
     end
 
     context "on #{os} with domainname and searchlist" do
       let(:params) do
-        { domainname: 'example.com', searchlist: %w[example.com example.net] }
+        { domainname: 'example.com', searchlist: ['example.com', 'example.net'] }
       end
 
       it {
@@ -260,27 +260,27 @@ describe 'resolv_conf' do
 
     context "on #{os} with valid parameter options" do
       let(:params) do
-        { options: %w[ndots:1 timeout:30 attempts:5] }
+        { options: ['ndots:1', 'timeout:30', 'attempts:5'] }
       end
 
       it {
-        is_expected.to contain_file('/etc/resolv.conf').
-          with_owner('root').
-          with_group('root').
-          with_mode('0644').
-          with_content(%r{^nameserver 127.0.0.1$}).
-          without_content(%r{^domain}).
-          without_content(%r{^search}).
-          without_content(%r{^sortlist$}).
-          with_content(%r{^options ndots:1$}).
-          with_content(%r{^options timeout:30$}).
-          with_content(%r{^options attempts:5$})
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_owner('root')
+          .with_group('root')
+          .with_mode('0644')
+          .with_content(%r{^nameserver 127.0.0.1$})
+          .without_content(%r{^domain})
+          .without_content(%r{^search})
+          .without_content(%r{^sortlist$})
+          .with_content(%r{^options ndots:1$})
+          .with_content(%r{^options timeout:30$})
+          .with_content(%r{^options attempts:5$})
       }
     end
 
     context "on #{os} with invalid parameter options" do
       let(:params) do
-        { options: %w[foo] }
+        { options: ['foo'] }
       end
 
       it {
